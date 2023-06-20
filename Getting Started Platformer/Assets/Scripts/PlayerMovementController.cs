a3eef1489d95c8c95fcb4e3ba8c814f847844c30
Joseph Collard <jcollard@users.noreply.github.com>
2023-06-20 14:14:06 -0700

    chore: Part 3.1

--- Getting Started Platformer/Assets/Scripts/PlayerMovementController.cs
+++ Getting Started Platformer/Assets/Scripts/PlayerMovementController.cs
@@ -6,11 +6,15 @@ public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 5;
+    private Rigidbody2D _rigidbody;
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
         float leftRight = Input.GetAxis("Horizontal");
-        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-        rigidbody.velocity = new Vector2(leftRight * _speed, rigidbody.velocity.y);
+        _rigidbody.velocity = new Vector2(leftRight * _speed, _rigidbody.velocity.y);
     }
 }

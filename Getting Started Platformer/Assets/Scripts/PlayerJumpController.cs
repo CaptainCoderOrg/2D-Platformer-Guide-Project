94b28ca5b20d0c37c87c83fa3db62703cf83fe45
Joseph Collard <jcollard@users.noreply.github.com>
2023-06-22 09:40:38 -0700

    chore: Part6.2

--- Getting Started Platformer/Assets/Scripts/PlayerJumpController.cs
+++ Getting Started Platformer/Assets/Scripts/PlayerJumpController.cs
@@ -7,17 +7,23 @@ public class PlayerJumpController : MonoBehaviour
     [SerializeField]
     private float _jumpPower = 15;
     private Rigidbody2D _rigidbody;
+    private BoxCollider2D _collider;
+    [SerializeField]
+    private float _distanceDelta;
     [SerializeField]
     private bool _isOnGround;
 
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distance = _collider.bounds.extents.y + _distanceDelta;
+        Debug.DrawRay(transform.position, Vector2.down * distance, Color.green, 1);
         if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
         {
             _rigidbody.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
